1513f6e31b1f673bfeb4db74bb423a6896ac9607
“jaketae” <jaesungtae@gmail.com>
2020-03-21 05:17:32 +0900

    Added alert function for email

--- app/scraper.py
+++ app/scraper.py
@@ -3,7 +3,10 @@ import requests
 import pandas as pd
 from app.utils import parse
 
+
+
 class Scraper:
+
 
 	def __init__(self, keyword):
 		keyword = parse(keyword)
@@ -12,18 +15,23 @@ class Scraper:
 		self.soup = bs4.BeautifulSoup(page.text, 'html.parser')
 		# self.soup = bs4.BeautifulSoup(page.text, 'lxml')
 
+
 	def _scrape_names(self):
 		self.names = []
 		for div in self.soup.findAll("div", {"class": "aafes-item-name"}):
 			self.names.append(div.find('a').contents[0])
+
 
 	def _scrape_prices(self):
 		self.prices = self.soup.findAll(
 			"div", {"class": ["aafes-price", "aafes-price-sale"]})
 		self.prices = list(filter(None, 
 			[element.text.strip().replace(' Sale', '') for element in self.prices]))
+		self.prices = [float(price) for price in self.prices]
+
 
 	def scrape(self):
 		self._scrape_names()
 		self._scrape_prices()
+		assert len(self.names) == len(self.prices)
 		self.summary = pd.DataFrame({"names": self.names, "prices": self.prices})
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,46 @@
+import time
+import ssl
+import smtplib
+
+
+
 def parse(keyword):
 	keyword = keyword.replace(' ', '&')
 	return keyword
+
+
+def alert(receiver_email):
+	port = 587
+	smtp_server = "smtp.gmail.com"
+	sender_email = "some_bot@gmail.com" # To-do: Make an account
+	password = "some_bot_password" # To-do: Make an account
+	# To-do: Add more info to email
+	message = """\
+	Subject: Price drop alert
+
+	The price of a product on your wish list has dropped.""" 
+	context = ssl.create_default_context()
+
+	try:
+		with smtplib.SMTP(smtp_server, port) as server:
+			server.ehlo()
+			server.starttls(context=context)
+			server.ehlo()
+			server.login(sender_email, password)
+			server.sendmail(sender_email, receiver_email, message)
+		print("==========Email sent!==========")
+	except Exception as e:
+		print(e)
+	finally:
+		server.quit()
+
+
+def check_price(table, threshold):
+	bool_idx = table['price'] < threshold
+	if sum(bool_idx):
+		lowered_table = table[bool_idx]
+		return lowered_table
+	else:
+		return False
+
+
