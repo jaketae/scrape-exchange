a28f63c74c3ba7691da252bf9a7eb126dcb00f77
victorkang99 <46831619+victorkang99@users.noreply.github.com>
2020-03-24 16:08:09 +0900

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -21,6 +21,31 @@ def organize(names, prices):
         if price[0] == '$':
             price_record[name] = price
     return price_record
-        
-    
-                         
+
+def send_alert(receiver_email):
+ 	port = 587
+ 	smtp_server = "smtp.gmail.com"
+	# To-do: Create bot email account
+	sender_email = "some_email@gmail.com"
+	sender_password = "some_password"
+	# To-do: Add more info to email
+	message = """\
+	Subject: Price alert
+	The price of a product on your wish list has dropped.""" 
+	context = ssl.create_default_context()
+	try:
+		with smtplib.SMTP(smtp_server, port) as server:
+			server.starttls(context=context)
+			server.login(sender_email, sender_password)
+			server.sendmail(sender_email, receiver_email, message)
+		print("==========Email sent!==========")
+	except Exception as e:
+		print(e)
+# To-do: convert string to float for price comparison
+def check_price(table, threshold):
+	bool_idx = table['price'] < threshold
+	if sum(bool_idx):
+		lowered_table = table[bool_idx]
+		return lowered_table
+	else:
+		return False
