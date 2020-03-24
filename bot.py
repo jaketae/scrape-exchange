94339ff5198f2fa1912aad1baf53a847c7052cb0
Victor Kang <victorkang99@berkeley.edu>
2020-03-24 20:24:01 +0900

    weekly update

--- bot.py
+++ bot.py
@@ -12,28 +12,35 @@ bot = Bot(ACCESS_TOKEN)
 
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
-    if request.method == 'GET':
-        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
-            return request.args.get("hub.challenge")
-        else:
-            return 'Invalid verification token'
-    else:
-        output = request.get_json()
-        for event in output['entry']:
-            messaging = event['messaging']
-            for message in messaging:
-                if message.get('message'):
-                    recipient_id = message['sender']['id']
-                    keyword = message['message']['text']
-                    scraper = Scraper(keyword)
-                    summary = scraper.scrape()
-                    bot.send_text_message(recipient_id, summary)
-                    specific_item = input("Which specific product are you interested in? ")
-                    
-                    receiver_email = input("What is your email address? ")
-                    send_alert(receiver_email)
-                    
-        return "Message Processed"
-    
+	if request.method == 'GET':
+		if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+			return request.args.get("hub.challenge")
+		else:
+			return 'Invalid verification token'
+	else:
+		output = request.get_json()
+		for event in output['entry']:
+			messaging = event['messaging']
+			for message in messaging:
+				if message.get('message'):
+					recipient_id = message['sender']['id']
+					keyword = message['message']['text']
+					scraper = Scraper(keyword)
+					summary = scraper.scrape()
+					table = scraper.bank()
+					bot.send_text_message(recipient_id, summary)
+					#Allows user to choose specific item 
+					specific_item = input("Which specific product are you interested in? ")
+					#Enter email address for future price drops
+					receiver_email = input("What is your email address? ")
+					#Weekly check to see if the price for that specific item dropped
+					while True:
+						price = check_price(specific_item)
+						if price < table[specific_item]: 
+							send_alert(receiver_email)
+						time.sleep(7 * 24 * 60 * 60)
+					
+		return "Message Processed"
+	
 if __name__ == "__main__":
-    app.run()
+	app.run()
