9f1f87b7dec68e7a34f13209fcfc240aaac7c774
Jake Tae <jaesungtae@gmail.com>
2020-06-11 23:46:10 +0900

    refactor: optimize by avoiding nested func

--- src/crawler.py
+++ src/crawler.py
@@ -28,42 +28,40 @@ def get_item_info(url):
     return title, floatify(price), url
 
 
-def get_summary(keyword):
-    def scrape_names(soup):
-        names = []
-        for div in soup.find_all("div", {"class": "aafes-item-name"}):
-            names.append(div.find("a").contents[0])
-        return names
+def scrape_names(soup):
+    names = []
+    for div in soup.find_all("div", {"class": "aafes-item-name"}):
+        names.append(div.find("a").contents[0])
+    return names
 
-    def scrape_prices(soup):
-        items = soup.find_all("div", {"class": "item-pricing mt-1"})
-        prices = []
-        for item in items:
+
+def scrape_prices(soup):
+    items = soup.find_all("div", {"class": "item-pricing mt-1"})
+    prices = []
+    for item in items:
+        try:
+            price = "".join(
+                item.find("div", {"class": "aafes-price-sale"})
+                .text.strip()
+                .replace(" Sale", "")
+                .split()
+            )
+        except:
             try:
-                price = "".join(
-                    item.find("div", {"class": "aafes-price-sale"})
-                    .text.strip()
-                    .replace(" Sale", "")
-                    .split()
-                )
+                price = item.find("div", {"class": "aafes-price"}).text.strip()
             except:
                 try:
-                    price = item.find("div", {"class": "aafes-price"}).text.strip()
+                    price = item.find("div", {"class": "aafes-price-sm"}).text.strip()
                 except:
-                    try:
-                        price = item.find(
-                            "div", {"class": "aafes-price-sm"}
-                        ).text.strip()
-                    except:
-                        price = "None"
-            prices.append(price)
-        return prices
+                    price = "None"
+        prices.append(price)
+    return prices
 
-    keyword = parse(keyword)
-    url = f"https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt={keyword}"
+
+def get_summary(keyword):
+    url = f"https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt={parse(keyword)}"
     page = requests.get(url)
     soup = bs4.BeautifulSoup(page.text, "lxml")
     names = scrape_names(soup)
     prices = scrape_prices(soup)
-    summary = stringify(names, prices)
-    return summary
+    return stringify(names, prices)
